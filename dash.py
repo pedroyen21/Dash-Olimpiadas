9a8c6b3fa85c42acfd424044500ff8ea053aa801
pedrosena21 <pedrosbhy93@gmail.com>
2021-09-17 16:04:38 -0300

    progesso

--- dash.py
+++ dash.py
@@ -1,13 +1,20 @@
 
 import plotly.express as px
-import numpy as np
+
 import pandas as pd
 
-dados_df = pd.read_excel(r"dados.xlsx")
+dados_df = pd.read_excel("dados.xlsx") #leitura dos dados
 
-for cells in dados_df.values:
-    print(cells[1])
 ##Posição	Atleta	Pais	Modalidade	Anos	Jogos	Sexo	Ouro	Prata	Bronze	Total
 
+atletas = dados_df['Atleta']
+medalhas = {}
 
+pd.options.display.max_rows=1000
+#print(int(dados_df['Ouro'][0]))
+
+print(dados_df[['Ouro','Prata','Bronze']])
+
+#for i in dados_df.values:
+ #   medalhas
 
