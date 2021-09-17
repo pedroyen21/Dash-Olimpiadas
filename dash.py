e0a514dce0e3175b32fa63b5de5ac9516ae257e5
pedrosena21 <pedrosbhy93@gmail.com>
2021-09-17 18:42:41 -0300

    Dicionarios
    Consegui colocar dicionários em uma lista. Cada dicionário é um atleta com nome, ouro, prata e bronze.

--- dash.py
+++ dash.py
@@ -1,20 +1,19 @@
 
 import plotly.express as px
-
 import pandas as pd
-
 dados_df = pd.read_excel("dados.xlsx") #leitura dos dados
 
 ##Posição	Atleta	Pais	Modalidade	Anos	Jogos	Sexo	Ouro	Prata	Bronze	Total
 
-atletas = dados_df['Atleta']
-medalhas = {}
+medalhas = []
 
-pd.options.display.max_rows=1000
-#print(int(dados_df['Ouro'][0]))
-
-print(dados_df[['Ouro','Prata','Bronze']])
-
-#for i in dados_df.values:
- #   medalhas
-
+for i in range(len(dados_df.values)):    
+    atleta = { 
+        'Nome':dados_df['Atleta'][i],
+        'Ouro': dados_df['Ouro'][i],
+        'Prata': dados_df['Prata'][i],
+        'Bronze': dados_df['Bronze'][i]
+    }
+    medalhas.append(atleta)
+print(medalhas)
+    
