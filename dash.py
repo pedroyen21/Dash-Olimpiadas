73d0e76393659fdd2da76db91ea188c20a1ad50d
pedrosena21 <pedrosbhy93@gmail.com>
2021-09-17 22:57:52 -0300

    Progresso

--- dash.py
+++ dash.py
@@ -5,15 +5,25 @@ dados_df = pd.read_excel("dados.xlsx") #leitura dos dados
 
 ##Posição	Atleta	Pais	Modalidade	Anos	Jogos	Sexo	Ouro	Prata	Bronze	Total
 
-medalhas = []
+medalhas = {}
+atletas = {}
 
-for i in range(len(dados_df.values)):    
-    atleta = { 
-        'Nome':dados_df['Atleta'][i],
-        'Ouro': dados_df['Ouro'][i],
-        'Prata': dados_df['Prata'][i],
-        'Bronze': dados_df['Bronze'][i]
+for i in range(len(dados_df.values)): 
+    
+    
+    medalhas = {       
+        'ouro' : dados_df['Ouro'][i],
+        'prata' : dados_df['Prata'][i],
+        'bronze' : dados_df['Bronze'][i]
     }
-    medalhas.append(atleta)
-print(medalhas)
+    atletas[dados_df['Atleta'][i].replace('\xa0', '')]= medalhas
+print(atletas.keys())
+ 
+#fig = px.bar(atletas, x=atletas.keys(), y=["atletas['ouro']", "atletas['prata']", "atletas['bronze']"], title="Wide-Form Input")
+#fig.show()
+    
+
+
+
+
     
